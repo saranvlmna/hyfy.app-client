4810fabe299d2fab8f00aadb25a5068c32139008
Saranjith <saranjith@metrictreelabs.com>
2023-05-23 01:06:15 +0530

    auth button init

--- lib/components/auth/authScreen.dart
+++ lib/components/auth/authScreen.dart
@@ -2,18 +2,40 @@
 
 import 'package:flutter/material.dart';
 
-
 class AuthScreen extends StatelessWidget {
   const AuthScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-          child: Text(
-        "Google Auth & Mobile auth",
-        textScaleFactor: 2,
-      )),
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.only(top: 30, bottom: 20, right: 100),
+              child: const Text(
+                " Continue with",
+                style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.w200,
+                    fontFamily: 'Ysabeau'),
+              ),
+            ),
+            Center(
+              child: Image.asset(
+                'assets/images/authimg.png',
+                height: 250,
+                width: 300,
+                alignment: Alignment.topLeft,
+              ),
+            ),
+            Container(
+              child: const Text("Google SignIn"),
+            ),
+            Container(child: Text("Mobile SignIn")),
+          ],
+        ),
+      ),
     );
   }
 }
