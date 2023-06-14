c74be0948e74e2e9daa18f9714a71ea39f05906a
Saranjith <saranjith@metrictreelabs.com>
2023-06-14 18:30:49 +0530

    url updated

--- lib/components/utilitys/constants.dart
+++ lib/components/utilitys/constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
-  static String baseUrl = 'https://364c-2402-3a80-4228-dcdb-f40f-756e-7749-d89f.ngrok-free.app';
+  static String baseUrl = 'https://hyfyserver.vercel.app';
   static String singninEndpoint = '/auth/signin';
   static String updateMobileEndpoint = '/auth/mobile/update';
   static String otpVerifyEndpoint = '/auth/otp/verify';
